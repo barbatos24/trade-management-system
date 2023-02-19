cc230fab96864927990d6994bc40f691c0a42274
zhoudewei <zhoudewei@shizhuang-inc.com>
2023-02-19 14:17:03 +0800

    系统初始化

---
+++ trade-interface/src/test/java/com/russellzhou/tradeinterface/TradeInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.russellzhou.tradeinterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeInterfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
