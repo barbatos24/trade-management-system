8ad22002503b52e11aeb205677b3ab6159a9f284
zhoudewei <zhoudewei@shizhuang-inc.com>
2023-02-19 16:01:53 +0800

    新增数据库连接

--- trade-interface/src/main/java/com/russellzhou/tradeinterface/TradeInterfaceApplication.java
+++ trade-interface/src/main/java/com/russellzhou/tradeinterface/TradeInterfaceApplication.java
@@ -3,7 +3,7 @@ package com.russellzhou.tradeinterface;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.russellzhou")
 public class TradeInterfaceApplication {
 
     public static void main(String[] args) {
